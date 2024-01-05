da9e84967bb7060dff8930ccc6a2e0c4e0cf4945
JavierCatalan <6703067-JavierCatalan@users.noreply.replit.com>
2024-01-05 19:36:53 +0000

    Initial commit

---
+++ dia1.py
@@ -0,0 +1,50 @@
+""" Día 1: Día de Programación (Día 1)
+PRINT singnifica = Mostrar 
+"""
+print("Practica 1\n")
+print("Python TOTAL - Programador Avanzado en 16 días\n")
+
+
+"""
+Práctica Print 1
+Crea un código que imprima en pantalla la expresión Me encanta estudiar Python.
+
+Aclaración: presta mucha atención al texto que debes reproducir, ya que errores ortográficos y de puntuación, espacios en blanco adicionales y saltos de línea innecesarios pueden ocasionar errores inesperados que no son detectados por el módulo de evaluación, devolviendo un error en tu ejercicio.
+"""
+
+#Solución Practica 1
+var = "Me encanta estudiar Python "
+print(var)
+
+"""Práctica Print 2
+Crea un código que imprima en pantalla la expresión Estudiar con "Python Total" es super divertido
+
+Aclaración: presta mucha atención al texto que debes reproducir, ya que errores ortográficos y de puntuación, espacios en blanco adicionales y saltos de línea innecesarios pueden ocasionar errores inesperados que no son detectados por el módulo de evaluación, devolviendo un error en tu ejercicio.
+"""
+#Solución Practica 2
+print("\n\n")
+print("Practica 2\n")
+print('Estudiar con "Python Total" es super divertido\n')
+
+"""Práctica Print 3
+Crea un código que imprima en pantalla el número 555, pero no debes imprimirlo directamente, sino como resultado de una operación matemática
+
+Aclaración: presta mucha atención al texto que debes reproducir, ya que errores ortográficos y de puntuación, espacios en blanco adicionales y saltos de línea innecesarios pueden ocasionar errores inesperados que no son detectados por el módulo de evaluación, devolviendo un error en tu ejercicio.
+"""
+#Solución Practica 3
+print("\n\n")
+var1= 500
+var2= 55
+total= var1 + var2
+print("Practica 3\n")
+print(total)
+
+#String
+print('Comillas simples\n') #Lineal
+print("Comillas dobles\n") #Lineal
+print("""Comillas triples
+Para parrafos
+con salto del lineas\n""") #Parrafos
+print("hola" + " " + 'J.C\n') #concatenando 3 cadenas
+print("me llamo  \"J.C\\n")#Comillas en cadena
+print("Esta es una linea\nY esta es otra") #Salto de linea
