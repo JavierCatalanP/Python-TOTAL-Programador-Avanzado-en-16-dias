ce4324a3a2b1f214e9f77fda29e8d70e6f3b6e70
JavierCatalanP <137312245+JavierCatalanP@users.noreply.github.com>
2024-01-12 13:00:27 +0000

    Operadores Lógicos

--- dia4.py
+++ dia4.py
@@ -20,24 +20,24 @@ Operadores de Comparación = se obtiene un valor booleano
 (==) Comparación
 
 """
-mi_bool = 5 + 5 == 10 - 2 + 10 #True   
-print(mi_bool)    
-mb2 = 'blanco'== 'blanco' #True
+mi_bool = 5 + 5 == 10 - 2 + 10  #True
+print(mi_bool)
+mb2 = 'blanco' == 'blanco'  #True
 print(mb2)
-mb3 = 'blanco'== 'negro' #False
+mb3 = 'blanco' == 'negro'  #False
 print(mb3)
-mb4 = 'blanco'== 'Blanco' #True
+mb4 = 'blanco' == 'Blanco'  #True
 print(mb4)
-mb5 =  'blanco'== 'Blanco'.lower() #True
-print(mi_bool)    
+mb5 = 'blanco' == 'Blanco'.lower()  #True
+print(mi_bool)
 print(mb5)
-mb6 = 'blanco'.upper() == 'blanCO' # 
+mb6 = 'blanco'.upper() == 'blanCO'  #
 print(mb6)
-mb7 = 'blanco'.upper() == 'blanco'.upper() #True
+mb7 = 'blanco'.upper() == 'blanco'.upper()  #True
 print(mb7)
-mb8 = '100' == 100 #False
+mb8 = '100' == 100  #False
 print(mb8)
-print9 = 100.0 == 100 #True
+print9 = 100.0 == 100  #True
 print(print9)
 
 #Práctica Operadores de Comparación 1
@@ -47,12 +47,12 @@ print("\n")
 Práctica Operadores de Comparación 1
 Crea dos variables (num1 y  num2) con los valores 36 y 17 respectivamente. Verifica si num1 es mayor o igual que num2 y almacena el resultado de dicha comparación en una variable llamada mi_bool
 """
-#Solución 
+#Solución
 num1 = 36
 num2 = 17
 
 mi_bool = num1 >= num2
-print (mi_bool)
+print(mi_bool)
 
 #Práctica Operadores de Comparación 2
 print('Práctica Operadores de Comparación 2')
@@ -68,8 +68,8 @@ Dentro de num2, almacena el número 5.
 
 Verifica si num1 es igual a num2 y almacena el resultado de dicha comparación en una variable llamada mi_bool."""
 
-#Solución 
-num1 = 25 ** 0.5
+#Solución
+num1 = 25**0.5
 num2 = 5
 
 mi_bool = num1 == num2
@@ -96,3 +96,121 @@ num2 = 64 * 3
 mi_bool = num1 != num2
 
 print(mi_bool)
+
+print("\n")
+#Operadores Logicos
+"""
+Y = and
+o = or
+no = not
+"""
+a = 10
+b = 5
+c = 2
+print(a > b and b < c)
+print("\n")
+
+mi_bool = 4 > 5 < 6
+print(mi_bool)
+print("\n")
+
+mi_bool = 4 > 5 and 5 < 6  #True
+print(mi_bool)
+print("\n")
+
+mi_bool = (55 == 55) and ('perro' == 'perro')  #True
+print(mi_bool)
+print("\n")
+
+mi_bool = 1 == 10 or 3 == 30  #False
+print(mi_bool)
+print("\n")
+
+texto = " esta frase es breve"
+mi_bool = 'esta' in texto  #True
+print(mi_bool)
+print("\n")
+
+mi_bool = ('esta' in texto) and ('breve' in texto)  #True
+print(mi_bool)
+print("\n")
+
+mi_bool = ('esta' in texto) or ('python' in texto)  #True
+print(mi_bool)
+print("\n")
+
+mi_bool = 'a' == 'a' and 'b' == 'b'  #True
+print(mi_bool)
+print("\n")
+
+mi_bool = not ('a' == 'a' and 'b' == 'b')  #False
+print(mi_bool)
+print("\n")
+
+mi_bool = not ('a' != 'a' and 'b' == 'b')  #True
+print(mi_bool)
+print("\n")
+
+#Práctica Operadores Lógicos 1
+
+"""
+Crea tres variables (num1 ,  num2 y num3):
+
+Dentro de num1, almacena el valor 36
+
+Dentro de num2, almacena el resultado de la operación 72/2
+
+Dentro de num3, almacena el valor 48
+
+Verifica si num1 es mayor que num2, y menor que num3. Almacena el resultado de dicha comparación en una variable llamada mi_bool.
+
+"""
+num1 = 36
+num2 = 72 / 2
+num3 = 48
+mi_bool = ((num1 > num2) and num3 )
+print(mi_bool)
+print("\n")
+#Práctica Operadores Lógicos 2
+"""
+
+Práctica Operadores Lógicos 2
+Crea tres variables (num1 ,  num2 y num3):
+
+Dentro de num1, almacena el valor 36
+
+Dentro de num2, almacena el resultado de la operación 72/2
+
+Dentro de num3, almacena el valor 48
+
+Verifica si num1 es mayor que num2, o menor que num3. Almacena el resultado de dicha comparación en una variable llamada mi_bool.
+"""
+num1 = 36
+num2 = 72 / 2
+num3 = 48
+mi_bool = (num1 > num2 or num2 < num3)
+print(mi_bool)
+print("\n") 
+
+#practica Operadores Lógicos 3
+"""
+Práctica Operadores Lógicos 3
+Verifica si las palabras almacenadas en las siguientes variables:
+
+palabra1 = "éxito", y
+
+palabra2 = "tecnología"
+
+no se encuentran en la frase a continuación, y almacena el resultado de esta comprobación (un booleano) en una variable llamada mi_bool:
+
+"Cuando algo es lo suficientemente importante, lo haces incluso si las probabilidades de que salga bien no te acompañan"
+
+Elon Musk"""
+
+frase = "Cuando algo es lo suficientemente importante, lo haces incluso si las probabilidades de que salga bien no te acompañan"
+palabra1 = "éxito"
+palabra2 = "tecnología"
+
+mi_bool = not(palabra1 == frase) and not(palabra2 == frase)
+print(mi_bool)
+print("\n")
