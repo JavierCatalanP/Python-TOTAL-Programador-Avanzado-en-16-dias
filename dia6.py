af398bcb74ca61897983517ba0055f5d9d20a1e7
Javier Catalán <ignaciocatalanp@gmail.com>
2024-01-29 00:44:28 -0300

    Abrir archivo

--- dia6.py
+++ dia6.py
@@ -1,4 +1,43 @@
+print('Día 6')
+
+"""
+- Abrir y manipular archivos
+- Crear archivos nuevos
+- Escribir y sobreescribir archivos
+- Navega entre directorios
+- Crear y eliminar carpetas
+"""
+print("Abrir y manipular archivos")
+
+""" E (Entrada) /S (Salida) - I (Input) / O (Output) 
+- abrir = open()
+- leer = read()
+- escribir = write()
+- cerrar = close()
+"""
+print("\nAbrir un archivo")
+mi_archivo = open('prueba.txt')
+print(mi_archivo.read()) #Abre el archivo
+
+print("\nReadline y rstrip\n")
+
+mi_archivo = open('prueba.txt')
+
+for l in mi_archivo:
+    print('Aquí dice: '+l)
+
+mi_archivo = open('prueba.txt')
+una_linea = mi_archivo.readline()
+
+una_linea = mi_archivo.readline()
+print(una_linea.upper())
+
+una_linea = mi_archivo.readline()
+print(una_linea.rstrip())
+
 mi_archivo = open('prueba.txt')
 todas = mi_archivo.readline()
-todas = todas
+#todas = todas.pop()
 print(todas)
+
+mi_archivo.close()# Cierra el archivo
