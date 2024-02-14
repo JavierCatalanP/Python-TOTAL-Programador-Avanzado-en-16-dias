3ff4342288ac8c89764af9b89b7e2eea97ed7262
Javier Catalán Peñaloza <137312245+JavierCatalanP@users.noreply.github.com>
2024-02-13 21:08:03 -0300

    POO

---
+++ dia7/practica_clases/poo.py
@@ -0,0 +1,48 @@
+"""
+¿Qué es ña Programación Orientada a Objetos (POO)?
+===============================================
+La programación orientada a objetos (POO) es un paradigma de programación.  que se basa en la creación de programas divididos en múltiples objetos. 
+
+Clases =  Plantillas para crear objetos. Son como recetas para preparar un plato, que contiene los ingredientes y las instrucciones para elaborarlo.
+
+ejemplo: Clase = Parajo{
+atributos como:
+color, tipo, habitat, edad, tamaño
+}
+métodos = funcionalidades como: volar, comer,  dormir.
+
+todos son objetos
+
+-------------------------------
+- Crear Clases
+- Definir atributos
+- Definir métodos
+- Crear objetos
+"""
+"""
+_______________________________________________________________
+6 Principios o Pilares bases de la Programación Orientada a Objetos
+
+- Herencia
+- Polimorfismo
+- Cohesión
+- Abstracción
+- Acoplamiento
+- Encapsulamiento
+
+Abstracción: Permite representar las características esenciales de un objeto, sin necesidad de conocer todos los detalles de su implementación [3].
+
+Encapsulamiento: Consiste en ocultar los detalles de implementación de un objeto y exponer solo la interfaz pública para interactuar con él [3].
+
+Herencia: Permite que una clase herede atributos y métodos de otra clase, facilitando la reutilización de código y la creación de jerarquías de clases [6].
+
+Polimorfismo: Permite que objetos de diferentes clases respondan al mismo método de diferentes maneras, lo que brinda flexibilidad y extensibilidad al código [6].
+
+Cohesión: Se refiere a la medida en que los elementos de una clase están relacionados y trabajan juntos para lograr un propósito común [6].
+
+Acoplamiento: Indica el grado de interdependencia entre las clases, donde se busca que las clases estén lo menos acopladas posible para promover la modularidad y la flexibilidad del código [6].
+-> Programar eventos -> Funciones -> Nace POO
+Permite agrupar  una serie de operaciones y datos en un solo lugar para poder manipularlos juntos.
+"""
+
+#https://ellibrodepython.com/programacion-orientada-a-objetos-python
