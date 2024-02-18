577950e0c589d1791eb0d5b279956061e5adb5de
JavierCatalanP <ingaciocatalanp@gmail.com>
2024-02-17 22:05:36 -0300

    Tipos de Métodos parte 1

--- dia7/practica_clases/4.-metodos.py
+++ dia7/practica_clases/4.-metodos.py
@@ -66,3 +66,46 @@ class Alarma: #Crea una instancia de la clase Alarma
         
         
         
+
+#Tipos de Métodos
+
+"""
+Decoradores:
+- Métodos de instancia = Son métodos visto anteriormente. Tienen las siguientes características:
+        * Acceden y modifican atributos del objeto.
+        * acceder a otros métodos.
+        * Modificar el estado de la clase.
+    Ejemplo:
+    
+    def mi_metodo(self):
+        print('algo')
+    mi_metodo()    
+    
+- Métodos de clase @classmethod = En segundo lugar, tenemos los métodos de clase.
+Estos se definen colocando antes, como dijimos arriba, class methods y en sus parámetros, en vez de poner self, ponemos CLS por clase.
+
+Estos métodos no están asociados a las instancias de nuestra clase, sino a la clase en sí misma.
+
+Por lo tanto, pueden ser llamados no solamente desde una instancia de nuestra clase, sino también directamente desde la clase.
+
+Estos métodos, a diferencia de los métodos de instancia, no pueden acceder a los atributos de instancia,pero sí pueden modificar, por supuesto, los atributos de la clase. Ejemplo:
+
+    @classmethod
+    def mi_metodo(cls): #"cls" por clase
+        print('algo')
+
+
+- Métodos estáticos @staticmethod = Se pueden definir con el decorador static method y no aceptan como parámetro ni self ni cls.
+
+Es por eso que no pueden modificar el estado ni de la clase ni de la instancia, pero por supuesto, pueden aceptar parámetros de entrada.
+
+Por lo tanto, el uso de los métodos estáticos puede resultar útil para indicar que un método no podrá modificar el estado de la instancia ni de la clase.
+
+En otras palabras, los métodos estáticos se podrían ver como funciones normales, de esas que usamos fuera de una clase, con la salvedad de que estos sí van ligados a una clase concreta.
+    Ejemplo:
+    
+    @staticmethod
+    def mi_metodo():
+        print('algo')
+
+"""
